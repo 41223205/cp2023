05c0231e7797ce168544784fd97b8a8d91862bc6
41223205 <144991637+41223205@users.noreply.github.com>
2023-12-13 08:28:56 +0000

    w12 上傳繪圖範例

--- downloads/hello.c
+++ downloads/hello.c
@@ -1,6 +1,9 @@
-  #include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    printf("hello world\n");
+    for(int i=0; i<10; i++){
+
+printf("第%d次印出 hello world\n", i );
+    }
 }
--- replit.nix
+++ replit.nix
@@ -1,13 +1,5 @@
 { pkgs }: {
-    deps = [
-      pkgs.libev
-      pkgs.gnuplot
-      pkgs.ncurses.dev
-      pkgs.gd
-    ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.libev
-    ];
-  };
+  deps = [
+    pkgs.gd
+  ];
 }
