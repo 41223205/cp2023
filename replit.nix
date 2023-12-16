f5e50492cc81815c57e1743832736a06df98677a
41223205 <144991637+41223205@users.noreply.github.com>
2023-12-16 09:40:54 +0000

    ANSIC新增01練習

--- replit.nix
+++ replit.nix
@@ -1,10 +1,5 @@
-{ pkgs }: {
-  deps = [
-    pkgs.libev
-  ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.libev
-    ];
-  };
+#include <stdio.h>
+int main(void){
+    // Here your code !
+
 }
