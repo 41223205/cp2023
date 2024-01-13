e45387a944e2e1dba0c799e188c79bb349b92a1d
41223205 <144991637+41223205@users.noreply.github.com>
2024-01-13 14:31:07 +0000

    新增頁面心得

---
+++ downloads/w16-1-2.c
@@ -0,0 +1,18 @@
+The intersection points (if any) between:
+A circle, center (3, -5) with radius 3, and:
+  a line containing the points (-10, 11) and (10, -9) is/are:
+    (6, -5)(3, -2)
+  a segment starting at (-10, 11) and ending at (-11, 12) is/are
+    []
+  a horizontal line containing the points (3, -2) and (7, -2) is/are:
+    (3, -2)
+A circle, center (0, 0) with radius 4, and:
+  a vertical line containing the points (0, -3) and (0, 6) is/are:
+    (0, 4)(0, -4)
+  a vertical segment starting at (0, -3) and ending at (0, 6) is/are:
+    (0, 4)
+A circle, center (4, 2) with radius 5, and:
+  a line containing the points (6, 3) and (10, 7) is/are:
+    (8, 5)(1, -2)
+  a segment starting at (7, 4) and ending at (11, 8) is/are:
+    (8, 5)
--- replit.nix
+++ replit.nix
@@ -1,8 +1,16 @@
 { pkgs }: {
-    deps = [
+  deps = [
+    pkgs.imagemagick
+    pkgs.libev
+    pkgs.gnuplot
+    pkgs.ncurses.dev
+    pkgs.gd
+    pkgs.freetype
+  ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH =
+pkgs.lib.makeLibraryPath [
       pkgs.libev
-      pkgs.gnuplot
-      pkgs.ncurses.dev
-      pkgs.gd
     ];
+  }
 }
